e291c1c8c0953df7aba845b0a4fc8f8896fb22ac
lauutarol <lautaro.lopez1997@gmail.com>
2021-08-10 23:09:51 -0300

    Hola

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
     console.log('Servidor funcionando');
 });
+
+
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
